5022451c3e372b7b80526efaf4d43ac61908028c
Alfredo Hernández <aldomann.designs@gmail.com>
2020-03-15 12:58:54 +0000

    Initial impementation of read_hadsst1() function

---
+++ R/read_hadisst1.R
@@ -0,0 +1,16 @@
+#' Title
+#'
+#' @param filename Path of netCDF file
+#' @param ... Arguments passed to raster::brick()
+#'
+#' @return RasterBrick object
+#' @export
+read_hadsst1 <- function(filename, ...) {
+  # Create RasterBrick object
+  raster_brick <- raster::brick(filename, ...)
+
+  # Set land masses as NA
+  raster::NAvalue(raster_brick) <- -32768
+
+  return(raster_brick)
+}
